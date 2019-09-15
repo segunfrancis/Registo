5bc9594a15b13808d4291f06387c92bfb3612dbc
segunfrancis <francis4dblues@gmail.com>
2019-09-15 16:20:14 +0100

    implementation: ViewPager adapter

--- app/src/main/java/com/android/segunfrancis/registo/MainActivity.java
+++ app/src/main/java/com/android/segunfrancis/registo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.android.segunfrancis.registo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpViewPager();
+    }
+
+    private void setUpViewPager() {
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        // add fragments to adapter
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new SignUpFragment());
+
+        // initiate viewPager
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(pagerAdapter);
+
+        // initiate tabLayout
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setText("Login");
+        tabLayout.getTabAt(1).setText("Sign Up");
     }
 }
---
+++ app/src/main/java/com/android/segunfrancis/registo/PagerAdapter.java
@@ -0,0 +1,35 @@
+package com.android.segunfrancis.registo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+}
